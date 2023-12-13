a33c91db45f9c0b8e53ba8e8b3b4819a6012f095
Willy Gutsche <willy.gutsche@unifesp.br>
2023-12-13 17:02:09 -0300

    Create login modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
 import Layout from '@/components/Layout';
+import LoginModal from '@/components/modals/LoginModal';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <LoginModal />
         <Layout>{children}</Layout>
       </body>
     </html>
